4fa1d3688edc81ecaf08e824ef4dbac149504970
Mikhail Cruz Andrade <jailandrade@gmail.com>
2016-07-12 23:06:12 -0500

    setup learnyoupython

---
+++ python/python/learnpythonthehardway/ex2.py
@@ -0,0 +1,2 @@
+# A comment, this is so you can read you program later
+print I could have code like this # and then this
---
+++ python/python/learnpythonthehardway/ex3.py
@@ -0,0 +1,3 @@
+print "I wil now count my chickens:"
+
+print "I have this eggs ", 20 + 23  
