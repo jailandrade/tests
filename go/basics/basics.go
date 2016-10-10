dffc685623ef230a9bc2445878d8e2607ba910e3
Mikhail Cruz Andrade <jailandrade@gmail.com>
2016-10-10 01:06:47 -0500

    learning go

---
+++ go/basics/basics.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hola mundo esto es desde Go")
+
+    var age int = 26
+
+    var pi float32 = 3.141592
+
+    fmt.Println(age, pi)
+}
