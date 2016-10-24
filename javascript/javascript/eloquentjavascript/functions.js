16c5479b1a3eeab4ac9980b5e3675ad8553b99f2
Mikhail Cruz Andrade <jailandrade@gmail.com>
2016-10-24 02:10:31 -0500

    some fun with javascript vainilla

---
+++ javascript/javascript/eloquentjavascript/functions.js
@@ -0,0 +1,19 @@
+var square = function(x) {
+    return x * x;
+};
+
+var makeNoise = function() {
+    console.log('zzzzzzzz');
+};
+
+var power = function(base, exponent) {
+    var result = 1;
+    for (var count = 0; count < exponent; count++) {
+	result *= base; 
+    }
+    return result;
+};
+
+console.log(square(12));
+makeNoise();
+console.log(power(2, 4));
