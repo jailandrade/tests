52020f05a0d1405db6b926d6a9d37a8e0a307d7b
Mikhail Cruz Andrade <jailandrade@gmail.com>
2016-10-25 01:19:04 -0500

    fun with php basics

---
+++ php/php/manual/basic-syntax.php
@@ -0,0 +1,34 @@
+<?php
+
+// PHP supports eight primitive types
+// Booleans
+// Integers
+// Floating point numbers
+// Strings
+// Arrays
+// Objects
+// Resources
+// Null
+// Callbacks
+
+// This is BOOLEAN and its value can be true or false
+$var_boolean = true;
+$var_boolean2 = false;
+
+// When converting to boolean the following values are false
+// the boolean val itself
+// the integer 0
+// the float 0
+// the empty string "" and the string "0"
+// an array with 0 elements
+// the special type NULL
+
+// An Integer is a number of the set z = { -2, -1, 0, 1, 2 }
+$var_integer = 1234;
+$var_integer2 = -1234;
+$var_integer_octal = 0123; // octal
+$var_integer_hexadecimal = 0x1A; // hexadecimal
+$var_integer_binary = 0b1111; // binary
+
+
+
---
+++ php/php/manual/types.php
@@ -0,0 +1,16 @@
+<?php
+echo "Hola mundo desde PHP\n";
+
+$expression = false;
+
+if ($expression == true) {
+    echo "Es cierto";
+} else {
+    echo "Es falso";
+}
+
+/*
+ * This is a comment
+ */
+
+// This is also a comment
